d0f08606727b1c16483901444596b60165371ef6
msesser <sessermatias@gmail.com>
2022-11-30 18:11:02 -0300

    Agrego botones con EventListener

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import freeCodeCampLogo from './imagenes/logo-freecodecamp.png';
+import Boton from './componentes/Boton.js'
+
+const manejarClick = () => {
+    console.log("click");
+  }
+
+  const reiniciarContador = () => {
+    console.log('reiniciar');
+  }
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='freecodecamp-logo-contenedor'>
+        <img className='freecodecamp-logo' src={freeCodeCampLogo} alt='logo-freecodecamp'/>
+      </div>
+      <div className='contenedor-principal'>
+        <Boton 
+          texto = 'Click'
+          esBotonDeClick = {true}
+          manejarClick = {manejarClick}/>
+        <Boton 
+          texto = 'Reiniciar'
+          esBotonDeClick = {false}
+          manejarClick = {reiniciarContador} />
+          
+      </div>
     </div>
   );
 }
---
+++ src/componentes/Boton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Boton({ texto , esBotonDeClick, manejarClick }){
+    return (
+        <button className = { esBotonDeClick ? 'boton-click' : 'boton-reiniciar' }
+                onClick = {manejarClick}>
+            {texto}
+        </button>
+    );
+}
+
+export default Boton
